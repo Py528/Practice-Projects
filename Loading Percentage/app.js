e0b4e7a40147b616d9485aaebad7574f91d75700
Py528 <pranaav.shinde5280@gmail.com>
2023-02-02 12:20:43 +0530

    Blurring Effect Completed

--- Loading Percentage/app.js
+++ Loading Percentage/app.js
@@ -1,10 +1,17 @@
 const loading_text = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
-
+let int = setInterval(blurring, 30);
 let load = 0;
-for (let i = 0; i < 100; i++) {
-  load++;
-  loading_text.innerText = `${load}%`;
-  
-  console.log(load);
+function blurring() {
+	load++;
+	if (load > 99) {
+		clearInterval(int);
+	}
+	loading_text.innerText = `${load}%`;
+	loading_text.style.opacity = scale(load, 0, 100, 1, 0);
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+	return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
